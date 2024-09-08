956cb33cc500e69f64c53d5f240bd677c32b0977
liminfinity <polieshko04@gmail.com>
2024-09-08 16:57:40 +0700

    chore: add alias and server at vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    open: true,
+  },
+  preview: {
+    port: 80,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
