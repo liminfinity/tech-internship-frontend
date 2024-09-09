4f7c15b1d218f374ca9596ea7d3af472f692cad3
liminfinity <polieshko04@gmail.com>
2024-09-09 13:32:17 +0700

    chore: move root to variable

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
