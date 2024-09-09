73635aec72b3fe5d9389ec030c741340c9c2a2f9
liminfinity <polieshko04@gmail.com>
2024-09-09 21:51:37 +0700

    feat: add advertisment api without endpoints

---
+++ src/entities/advertisment/api/consts.ts
@@ -0,0 +1,10 @@
+export const REDUCER_API_PATH = 'advertismentApi';
+
+export const BASE_URL = 'advertisments';
+
+export const KEEP_UNUSED_DATA_FOR = 10 * 60; // 10 minutes
+
+export const API_PATHS = {
+  GET_ADVERTISMENTS: BASE_URL,
+  GET_ADVERTISMENT: BASE_URL,
+} as const;
--- src/entities/advertisment/index.ts
+++ src/entities/advertisment/index.ts
@@ -1,1 +1,3 @@
 export type { Advertisment, ShortAdvertisment } from './types';
+
+export { REDUCER_API_PATH, advertismentApi } from './api';
